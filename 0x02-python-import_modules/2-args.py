2c690976fd891a348088718503c5245acf93d02f
BrianSechelo <sechbrian123@gmail.com>
2024-01-05 08:34:30 -0800

    Task 2 done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+len = len(argv) - 1
+if len < 1:
+    print("{} arguments.".format(len))
+elif len == 1:
+    print("{} argument.".format(len))
+else:
+    print("{} arguments".format(len))
+
+for i in range(len):
+    print("{}: {:s}".format(i + 1, argv[i + 1]))
