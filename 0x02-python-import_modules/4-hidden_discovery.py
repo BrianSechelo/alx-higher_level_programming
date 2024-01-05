151a577e0758a81b13eabd5e1dc00dfc20e3514c
BrianSechelo <sechbrian123@gmail.com>
2024-01-05 12:14:43 -0800

    edit to 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    from hidden_4 import*
     allnames = dir()
-    for i in range (0, len(allnames)):
+    for i in range(0, len(allnames)):
         if allnames[i][:2] != "__":
             print("{:s}".format(allnames[i]))
