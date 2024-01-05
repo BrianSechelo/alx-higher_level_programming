cc213c2ae66aa427dd521fc9a4c9c8600d5b474b
BrianSechelo <sechbrian123@gmail.com>
2024-01-05 11:12:38 -0800

    Edits to 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    lenn = len(argv)
-    sumall = 0;
+    sumall = 0
     for i in range(1, lenn):
         sumall += int(argv[i])
         print("{}".format(sumall))
