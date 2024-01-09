adcbfb0a7a5cb9293b27a7009f587a5959784c18
BrianSechelo <sechbrian123@gmail.com>
2024-01-09 03:21:27 -0800

    Task 5 edited

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    if my_string[:]:
-        new_string = my_string.translate({ord("c"):None})
-        second_string = new_string.translate({ord("C"):None})
-        return second_string
+    for j in my_string:
+        if (j == "c") or (j == "C"):
+            my_string = my_string[:my_string.index(j)] \
+                    + my_string[my_string.index(j) + 1:]
+
     return my_string
