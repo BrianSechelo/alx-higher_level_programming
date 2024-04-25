74a376cbee039fef6c6668b529cff368543a06b3
BrianSechelo <sechbrian123@gmail.com>
2024-04-25 15:14:04 -0700

    Tasks 0 and 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if the user has provided a URL
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Fetch the response body and count its bytes
-response_size=$(curl -s "$1" | wc -c)
-
-# Display the size of the response body in bytes
-echo "$response_size"
+# This script displays the size of the body of the response in bytes
+curl -s "$1" | wc -c
