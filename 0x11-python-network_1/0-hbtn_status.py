700c1a5ba8866fbda411f66cebbd988d734f4f89
BrianSechelo <sechbrian123@gmail.com>
2024-04-26 12:14:14 -0700

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script fetches https://alx-intranet.hbtn.io/status"""
+
+from urllib.request import Request, urlopen
+from urllib.error import URLERROR
+
+def fetch_body():
+    try:
+        req = Request("https://alx-intranet.hbtn.io/status")
+        with urlopen(req) as response:
+            body =  response.read()
+        print("Body Response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print(e.reason)
+        if hasattr(e, 'code'):
+            print(e.code)
+
+if __name__ == "__main__":
+    fetch_body()
+
